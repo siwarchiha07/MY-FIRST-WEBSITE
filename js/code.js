62972a48e5edeaa1f5a2cf843b34c94a8fb169b8
siwar chiha <islamsiwar.chiha@polytechnicien.tn>
2023-11-22 11:20:21 +0100

    validation depuis working directory

---
+++ js/code.js
@@ -0,0 +1,5 @@
+function calcul(){
+    let a=10;
+    let b=a*0.02;
+    alert("le resultat est egal:" +b);
+}
---
+++ traitement.php
@@ -0,0 +1,8 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$db="poldata";
+$name=$_post['name'];
+
+?>
